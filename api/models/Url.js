274becce7fb9b5c2982851773f06da8d03aa0bde
Siraj <sirajmhanna@hotmail.com>
2022-01-20 12:42:55 +0200

    Create Url.js

---
+++ api/models/Url.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const URLSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    originalUrl: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    shortUrl: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    visitorsCount: Number,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("url", URLSchema);
