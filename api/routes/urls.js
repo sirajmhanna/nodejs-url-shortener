afd1fe0ec6157736880658098886213ff2805297
Siraj <sirajmhanna@hotmail.com>
2022-01-20 12:43:01 +0200

    Create urls.js

---
+++ api/routes/urls.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const route = express.Router();
+const RequestsMiddleware = require("../middlewares/requests");
+const UrlsControllers = require("../controllers/urls");
+
+// URL Shortener Route
+route.post(
+  "/shortener",
+  RequestsMiddleware.generateRequestIdentifier,
+  UrlsControllers.shortener
+);
+
+// Original URL Route
+route.get(
+  "/",
+  RequestsMiddleware.generateRequestIdentifier,
+  UrlsControllers.getOriginalUrl
+);
+
+module.exports = route;
